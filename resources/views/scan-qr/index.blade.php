44c1641993bed7e1641887bdb2ad953b1db82dae
S1nt4x16 <wibisonogetar@gmail.com>
2023-02-01 19:29:37 +0700

    SELESAII PARAHH

--- resources/views/scan-qr/index.blade.php
+++ resources/views/scan-qr/index.blade.php
@@ -60,7 +60,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">From Keterangan</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Form Keterangan</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="close"></button>
       </div>
         <form action="{{ route('store') }}" method="post">
