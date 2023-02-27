753ba39ab01441ad04797335073b961741a04d27
S1nt4x16 <wibisonogetar@gmail.com>
2023-02-27 19:13:01 +0700

    P

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,90 +11,189 @@ class MainController extends Controller
 {
     public function index() 
     {
-
         return view('scan-qr.index');
     }
 
     public function store(Request $request) 
     {    
         $id = $request->input('id');
+        $nrp = $request->input('nrp');
         $piket = $request->input('piket');
         $keterangan = $request->input('keterangan');
 
-        $find = DB::table('tm_anggota')
+
+        if($id) 
+        {
+
+            $find = DB::table('tm_anggota')
             ->where('id', $id)
             ->count();
         
-        if(!$find) {
-            return redirect()->back()->with('error', 'QR Code Anda Tidak Ditemukan');   
+        
+            if(!$find) {
+                return redirect()->back()->with('error', 'QR Code Anda Tidak Ditemukan');   
+            }
+
+            $day = Carbon::today()->toDateString();
+
+            $count = DB::table('presensi')
+                ->where('id_anggota', $id)
+                ->whereDate('created_at', $day)
+                ->count();
+
+            if($piket == 'on') {
+                $piketvalue = 'Y';
+            } else {
+                $piketvalue = 'N';
+            }
+
+            if($count == 0) {
+            $id_data = DB::table('presensi')
+                ->insertGetId([
+                    'id_anggota' => $id,
+                    'piket' => $piketvalue,
+                    'keterangan' => $keterangan,
+                    'status' => 'in',
+                    'created_at' => now()
+                    
+                ]);
+
+            $show = DB::table('presensi as a')
+                ->join('tm_anggota as b', 'b.id', '=', 'a.id_anggota')
+                ->select("b.name", "b.jekel", "b.kelas", "b.angkatan", "b.nrp", "a.status", "a.piket", "a.keterangan")
+                ->where('a.id', $id_data)
+                ->first();
+            }
+
+            if($count == 1) {
+            $id_data = DB::table('presensi')
+                ->insertGetId([
+                    'id_anggota' => $id,
+                    'piket' => $piketvalue,
+                    'keterangan' => $keterangan,
+                    'status' => 'out',
+                    'created_at' => now()
+                    
+                ]);
+
+            $show = DB::table('presensi as a')
+                ->join('tm_anggota as b', 'b.id', '=', 'a.id_anggota')
+                ->select("b.name", "b.jekel", "b.kelas", "b.angkatan", "b.nrp", "a.status", "a.piket", "a.keterangan")
+                ->where('a.id', $id_data)
+                ->first();
+            }
+            
+            if($count >= 2) {
+                
+            return redirect()->back()->with('error', 'GAUSAH ABSEN LAGI LU UDAH ABSEN PULANG TADI :)');
+            }
+            
+            if($show->jekel = 'L') {
+                $jekelvalue = 'Laki-Laki';
+            } else {
+                $jekelvalue = 'Perempuan';
+            }
+
+            if($show->status == 'in') {
+                $statusvalue = 'Masuk';
+            } else {
+                $statusvalue = 'Keluar';
+            }
+
+            return redirect()->back()->with('success', 'Selamat Anda Telah Berhasil Melakukan Presensi'. ' '. $show->name. ' '. 'Anda Telah'. ' ' . $statusvalue);
         }
 
-        $day = Carbon::today()->toDateString();
-
-        $count = DB::table('presensi')
-            ->where('id_anggota', $id)
-            ->whereDate('created_at', $day)
+        if($nrp) 
+        {
+        
+            $findNrp = DB::table('tm_anggota')
+            ->where('nrp', $nrp)
             ->count();
 
-        if($piket == 'on') {
-            $piketvalue = 'Y';
-        } else {
-            $piketvalue = 'N';
-        }
 
-        if($count == 0) {
-        $id_data = DB::table('presensi')
-            ->insertGetId([
-                'id_anggota' => $id,
-                'piket' => $piketvalue,
-                'keterangan' => $keterangan,
-                'status' => 'in',
-                'created_at' => now()
-                
-            ]);
+            if(!$findNrp) {
+                return redirect()->back()->with('error', 'NRP Anda Tidak Ditemukan');  
+            }
 
-        $show = DB::table('presensi as a')
-            ->join('tm_anggota as b', 'b.id', '=', 'a.id_anggota')
-            ->select("b.name", "b.jekel", "b.kelas", "b.angkatan", "b.nrp", "a.status", "a.piket", "a.keterangan")
-            ->where('a.id', $id_data)
-            ->first();
-        }
+            $day = Carbon::today()->toDateString();
 
-        if($count == 1) {
-        $id_data = DB::table('presensi')
-            ->insertGetId([
-                'id_anggota' => $id,
-                'piket' => $piketvalue,
-                'keterangan' => $keterangan,
-                'status' => 'out',
-                'created_at' => now()
-                
-            ]);
+            $countNrp = DB::table('tm_anggota')
+                ->where('nrp', $nrp)
+                ->whereDate('created_at', $day)
+                ->count();
 
-        $show = DB::table('presensi as a')
-            ->join('tm_anggota as b', 'b.id', '=', 'a.id_anggota')
-            ->select("b.name", "b.jekel", "b.kelas", "b.angkatan", "b.nrp", "a.status", "a.piket", "a.keterangan")
-            ->where('a.id', $id_data)
-            ->first();
-        }
-        
-        if($count >= 2) {
+            $idd = DB::table('tm_anggota')
+                ->select('id')
+                ->where('nrp', $nrp)
+                ->first();
             
-           return redirect()->back()->with('error', 'GAUSAH ABSEN LAGI LU UDAH ABSEN PULANG TADI :)');
+            if($piket == 'on') {
+                $piketvalue = 'Y';
+            } else {
+                $piketvalue = 'N';
+            }
+
+            if($countNrp === 0) 
+            {
+                $nrp_data = DB::table('presensi')
+                    ->insertGetId([
+                        'id_anggota' => $idd->id,
+                        'piket' => $piketvalue,
+                        'keterangan' => $keterangan,
+                        'status' => 'in',
+                        'created_at' => now()
+                        
+                    ]);
+    
+                $show = DB::table('presensi as a')
+                    ->join('tm_anggota as b', 'b.id', '=', 'a.id_anggota')
+                    ->select("b.name", "b.jekel", "b.kelas", "b.angkatan", "b.nrp", "a.status", "a.piket", "a.keterangan")
+                    ->where('a.id', $nrp_data)
+                    ->first();
+            }
+
+            if($countNrp === 1) 
+            {
+                $nrp_data = DB::table('presensi')
+                    ->insertGetId([
+                        'id_anggota' => $idd->id,
+                        'piket' => $piketvalue,
+                        'keterangan' => $keterangan,
+                        'status' => 'out',
+                        'created_at' => now()
+                        
+                    ]);
+    
+                $show = DB::table('presensi as a')
+                    ->join('tm_anggota as b', 'b.id', '=', 'a.id_anggota')
+                    ->select("b.name", "b.jekel", "b.kelas", "b.angkatan", "b.nrp", "a.status", "a.piket", "a.keterangan")
+                    ->where('a.id', $nrp_data)
+                    ->first();
+            }
+                
+            if($countNrp >= 2) 
+            {
+                    
+                return redirect()->back()->with('error', 'GAUSAH ABSEN LAGI LU UDAH ABSEN PULANG TADI :)');
+
+            }
+
+                if($show->jekel = 'L') {
+                    $jekelvalue = 'Laki-Laki';
+                } else {
+                    $jekelvalue = 'Perempuan';
+                }
+    
+                if($show->status == 'in') {
+                    $statusvalue = 'Masuk';
+                } else {
+                    $statusvalue = 'Keluar';
+                }
+    
+                return redirect()->back()->with('success', 'Selamat Anda Telah Berhasil Melakukan Presensi'. ' '. $show->name. ' '. 'Anda Telah'. ' ' . $statusvalue);
         }
+
+        return redirect()->back()->with('gagal', 'ID dan NRP tidak ditemukan');
         
-        if($show->jekel = 'L') {
-            $jekelvalue = 'Laki-Laki';
-        } else {
-            $jekelvalue = 'Perempuan';
-        }
-
-        if($show->status == 'in') {
-            $statusvalue = 'Masuk';
-        } else {
-            $statusvalue = 'Keluar';
-        }
-
-        return redirect()->back()->with('success', 'Selamat Anda Telah Berhasil Melakukan Presensi'. ' '. $show->name. ' '. 'Anda Telah'. ' ' . $statusvalue);
     }
 }
--- resources/views/scan-qr/index.blade.php
+++ resources/views/scan-qr/index.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Presensi Kopasus IT</title>
     <link rel="stylesheet" href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sweetalert2/dist/sweetalert2.min.css') }}">
     <style>
         body {
             background-image: url('{{ asset('images/p.jpeg') }}');
@@ -44,10 +45,21 @@
           <div id="reader" style="width:600px;"></div>
         </div>
         <div class="card-footer">
+            <center><p><b>Atau</b></p></center>
+            <center><p><b>Masukkan NRP Manual</b></p></center>
+            <div class="row">
+              <div class="col-10">
+                <input type="text" name="nrpInput" id="nrpInput" class="form-control mb-3 bg-dark" style="color: white;" placeholder="Masukkan NRP" required autofocus> 
+              </div>
+              <div class="col-2">
+                <button type="submit" onclick="OnInputNrp()" class="btn btn-success">OK</button>
+              </div> 
+            </div>
             <center><p><b>Ceklis Bagian Piket Jika Kalian Piket Hari Ini.</b></p></center> 
         </div>
 </div>
-<input type="hidden" name="result" id="result">
+
+<input type="hidden" name="result" id="result"> 
 
 <div class="modal fade" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -77,9 +89,38 @@
   </div>
 </div>
 
+<div class="modal fade" id="modalForm2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Form Keterangan</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="close"></button>
+      </div>
+        <form action="{{ route('store') }}" method="post">
+            @csrf
+            <input type="hidden" name="nrp" id="nrpInput2">
+            <div class="mb-4 mt-4">
+                <center>
+                    <input type="checkbox" name="piket" id="piket">
+                    <label for="piket">Ceklis Bagian Ini Jika Kalian Piket</label>
+                </center>
+            </div>
+            <div class="m-4">
+                <input type="text" class="form-control" name="keterangan" placeholder="Masukan Keterangan Apa Hasil Eksplorasi Kalian" id="exampleInputPassword1" required autofocus>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" id="batal" data-bs-dismiss="modal">Batal</button>
+              <button type="submit" class="btn btn-success">OK!</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
+
 <script src="{{ asset('bootstrap/dist/js/bootstrap.bundle.min.js' )}}"></script>
 <script src="{{ asset('html5-qrcode/html5-qrcode.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('jquery-3.6.3.min.js') }}"></script>
+<script src="{{ asset('sweetalert2/dist/sweetalert2.min.js') }}"></script>
 <script>
     
     $('#close').click(function() {
@@ -89,6 +130,31 @@
     $('#batal').click(function() {
       location.reload();
     });
+
+ 
+
+    function OnInputNrp() 
+    {
+
+        if(document.getElementById("nrpInput").value.length == 0)
+        {
+          
+          Swal.fire({
+            title: 'Error!',
+            text: 'Masukan NRP!',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          })
+
+        } else {
+
+          var nrp = $('#nrpInput').val();
+          $('#nrpInput2').val(nrp);
+          $('#modalForm2').modal('show');
+        }
+
+    }
+
 
     function onScanSuccess(decodedText, decodedResult) {
         
